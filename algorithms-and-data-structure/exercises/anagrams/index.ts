5488e50abeb33edcd04cf21c95d9cc5eec35cecd
Theodore Chuang <chuangtheodore@gmail.com>
2020-03-20 18:12:41 +1100

    [algo] solutions for anagrams, capitalize

--- algorithms-and-data-structure/exercises/anagrams/index.ts
+++ algorithms-and-data-structure/exercises/anagrams/index.ts
@@ -1,5 +1,5 @@
 // --- Directions
-// Check to see if two provided strings are anagrams of eachother.
+// Check to see if two provided strings are anagrams of each other.
 // One string is an anagram of another if it uses the same characters
 // in the same quantity. Only consider characters, not spaces
 // or punctuation.  Consider capital letters to be the same as lower case
@@ -8,10 +8,58 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-// "Hi there!!".replace(/[^\w]/g, "") => "Hithere"
-
-function anagrams(stringA: String, stringB: String) {
-  return false
+function stringToOnlyLowerChar(string: String) {
+  return string.replace(/[^\w]/g, '').toLowerCase()
 }
 
-export { anagrams }
+function stringToCharMap(string: String) {
+  const charMap: { [key: string]: number } = {}
+
+  for (const char of string.split('')) {
+    charMap[char] = charMap[char] + 1 || 1
+  }
+
+  return charMap
+}
+
+/**
+ * Check to see if two provided strings are anagrams of each other
+ * One string is an anagram of another if it uses the same character in the same quantity.
+ * Only consider characters, not space or punctuation.  Consider capital letters to be the same as lower case
+ */
+function anagrams1(stringA: String, stringB: String) {
+  const charMapA = stringToCharMap(stringToOnlyLowerChar(stringA))
+  const charMapB = stringToCharMap(stringToOnlyLowerChar(stringB))
+
+  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+    return false
+  }
+
+  for (const char in charMapA) {
+    if (charMapA[char] !== charMapB[char]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+/**
+ * Check to see if two provided strings are anagrams of each other
+ * One string is an anagram of another if it uses the same character in the same quantity.
+ * Only consider characters, not space or punctuation.  Consider capital letters to be the same as lower case
+ */
+function anagrams2(stringA: String, stringB: String) {
+  const charsA = stringToOnlyLowerChar(stringA)
+    .split('')
+    .sort()
+    .join('')
+  const charsB = stringToOnlyLowerChar(stringB)
+    .split('')
+    .sort()
+    .join('')
+
+  return charsA === charsB
+}
+
+export { anagrams1, anagrams2 }
--- algorithms-and-data-structure/exercises/anagrams/test.ts
+++ algorithms-and-data-structure/exercises/anagrams/test.ts
@@ -1,25 +1,31 @@
-import { anagrams } from './index'
+import { anagrams1, anagrams2 } from './index'
 
 test('anagrams function exists', () => {
-  expect(typeof anagrams).toEqual('function')
+  expect(typeof anagrams1).toEqual('function')
+  expect(typeof anagrams2).toEqual('function')
 })
 
 test('"hello" is an anagram of "llohe"', () => {
-  expect(anagrams('hello', 'llohe')).toBeTruthy()
+  expect(anagrams1('hello', 'llohe')).toBeTruthy()
+  expect(anagrams2('hello', 'llohe')).toBeTruthy()
 })
 
 test('"Whoa! Hi!" is an anagram of "Hi! Whoa!"', () => {
-  expect(anagrams('Whoa! Hi!', 'Hi! Whoa!')).toBeTruthy()
+  expect(anagrams1('Whoa! Hi!', 'Hi! Whoa!')).toBeTruthy()
+  expect(anagrams2('Whoa! Hi!', 'Hi! Whoa!')).toBeTruthy()
 })
 
 test('"One One" is not an anagram of "Two two two"', () => {
-  expect(anagrams('One One', 'Two two two')).toBeFalsy()
+  expect(anagrams1('One One', 'Two two two')).toBeFalsy()
+  expect(anagrams2('One One', 'Two two two')).toBeFalsy()
 })
 
 test('"One one" is not an anagram of "One one c"', () => {
-  expect(anagrams('One one', 'One one c')).toBeFalsy()
+  expect(anagrams1('One one', 'One one c')).toBeFalsy()
+  expect(anagrams2('One one', 'One one c')).toBeFalsy()
 })
 
 test('"A tree, a life, a bench" is not an anagram of "A tree, a fence, a yard"', () => {
-  expect(anagrams('A tree, a life, a bench', 'A tree, a fence, a yard')).toBeFalsy()
+  expect(anagrams1('A tree, a life, a bench', 'A tree, a fence, a yard')).toBeFalsy()
+  expect(anagrams2('A tree, a life, a bench', 'A tree, a fence, a yard')).toBeFalsy()
 })

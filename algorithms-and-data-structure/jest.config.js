b083eb36dd68648ca899c315653dc3b167e9cbc7
Theodore Chuang <chuangtheodore@gmail.com>
2020-03-29 17:09:37 +1100

    chore(algo): move config files to rootDir. Add debug config for test

--- algorithms-and-data-structure/jest.config.js
+++ algorithms-and-data-structure/jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  roots: ['<rootDir>'],
+  roots: ['<rootDir>/exercises'],
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest'
-  }
+  },
+  collectCoverage: true
 }
